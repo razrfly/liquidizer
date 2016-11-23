15ac0bdd779fa5d09f73e730167824e24ecc5d46
Szymon Madeja <smadeja1@gmail.com>
2016-11-23 16:06:06 +0100

    Make sure finders won't break for blank names

--- lib/liquidizer/liquid_template.rb
+++ lib/liquidizer/liquid_template.rb
@@ -6,15 +6,19 @@ module Liquidizer
 
     module ClassMethods
       def find_by_name(name)
-        find_by(:name => name) || find_default_by_name(name)
+        if name.present?
+          find_by(:name => name) || find_default_by_name(name)
+        end
       end
 
       def find_default_by_name(name)
-        Liquidizer.template_paths.each do |path|
-          file_name = File.join(path, name) + '.liquid'
+        if name.present?
+          Liquidizer.template_paths.each do |path|
+            file_name = File.join(path, name) + '.liquid'
 
-          if File.exist?(file_name)
-            return new(:name => name, :content => File.read(file_name))
+            if File.exist?(file_name)
+              return new(:name => name, :content => File.read(file_name))
+            end
           end
         end
 
